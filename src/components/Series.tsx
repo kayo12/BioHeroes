f06756f6878c0d2084ca2113d527cf17f3367d24
Kayo campos <kayonbahenrique@gmail.com>
2023-01-21 20:51:50 -0300

    Correção de url

--- src/components/Series.tsx
+++ src/components/Series.tsx
@@ -83,7 +83,7 @@ export default function Series(props?: Series) {
 
   useEffect(() => {
     fetch(
-      `http://gateway.marvel.com/v1/public/series?ts=1&limit=60&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_API_HASH}`
+      `https://gateway.marvel.com/v1/public/series?ts=1&limit=60&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_API_HASH}`
     )
       .then((response) => {
         return response.json();
@@ -112,7 +112,7 @@ export default function Series(props?: Series) {
         <ul className="PopBody">
           {series.map((current, index) => {
             return current.thumbnail.path + "." + current.thumbnail.extension !=
-              "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" ? (
+              "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" ? (
               <li key={index}>
                 <Cards
                   title={current.title}
