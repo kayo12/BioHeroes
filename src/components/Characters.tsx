571c6f98f9643b0c94c667985bd60eb10b95f973
Kayo campos <kayonbahenrique@gmail.com>
2023-02-07 01:05:42 -0300

    feat(characters): add função de paginação e exibição com base na quantidade de personagens

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -41,33 +41,32 @@ const CharacterSection = styled.section`
     display: flex;
   }
 
-  .Character-Paging{
+  .Character-Paging {
     display: flex;
     justify-content: center;
     width: 100%;
   }
 
-  .Character-Paging a{
+  .Character-Paging a {
     text-decoration: none;
-     padding: 8px 16px;
-     color: #000000;
-      font-weight: bold;
+    padding: 8px 16px;
+    color: #000000;
+    font-weight: bold;
   }
   .Character-Paging a:hover {
-      
-      background-color: #ff3700;
-      color: #fff;
-      font-weight: bold;
+    background-color: #ff3700;
+    color: #fff;
+    font-weight: bold;
   }
-
 `;
 
 export default function Characters(props) {
   const [char, setChar] = useState([]);
+  const [pages, setPages] = useState(undefined)
 
   useEffect(() => {
     fetch(
-      `https://gateway.marvel.com/v1/public/characters?ts=1&limit=8&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_API_HASH}`
+      `https://gateway.marvel.com/v1/public/characters?ts=1&limit=30&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_API_HASH}`
     )
       .then((response) => {
         return response.json();
@@ -81,32 +80,43 @@ export default function Characters(props) {
       });
   }, []);
 
+  const groups = () => {
+
+    let last = Number(pages?.target.value * 8);
+    let first = 8 - (Number(pages?.target.value * 8));
+    let gp = char.slice(first, last).map((current, index) => {
+      return (
+        <Cards
+          key={`${index}`}
+          title={current.name}
+          width={"200px"}
+          height={"200px"}
+          image={current.thumbnail.path + "." + current.thumbnail.extension}
+        />
+      );
+    });
+
+    return gp;
+  };
+
+  const pagingLength = () => {
+    let group = Math.round(char.length / 8);
+    let pading = [...Array(group)].map((_, indx) => {
+      return (
+        <a href="" key={`${indx}_`} onClick={(current) => setPages(current)}>
+          {indx + 1}
+        </a>
+      );
+    });
+    return pading;
+  };
+
   return (
     <CharacterSection>
       <div className="Character-Container">
         <h3 className="Character-Header">Personagens</h3>
-        <div className="Character-Body">
-          {char.map((current, index) => {
-            return (
-              <Cards
-                key={index}
-                title={current.name}
-                width={"200px"}
-                height={"200px"}
-                image={
-                  current.thumbnail.path + "." + current.thumbnail.extension
-                }
-              />
-            );
-          })}
-        </div>
-        <div className="Character-Paging">
-           <a href="">1</a>
-           <a href="">2</a>
-           <a href="">3</a>
-           <a href="">4</a>
-           <a href="">5</a> 
-      </div>
+        <div className="Character-Body">{groups()}</div>
+        <div className="Character-Paging">{pagingLength()}</div>
       </div>
     </CharacterSection>
   );
